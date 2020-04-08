96f0ca1ff512e1efad945054e7afa338613ea1b0
trumpet63-stream <trumpet63streaming@gmail.com>
2020-04-08 19:01:43 -0400

    vertical radio buttons in a scroll div

--- src/scripts2/page_1.ts
+++ src/scripts2/page_1.ts
@@ -13,32 +13,73 @@ export abstract class Page1 {
     public static draw() {
         drawHeading();
         let p: p5 = global.p5Scene.sketchInstance;
+        p.push();
+        p.textAlign(p.CENTER);
 
-        let stepfileInput = DOMWrapper.create(() => {
-            return p.createFileInput(global.simfile.load.bind(global.simfile), "false");
-        }, "setfileInputButton").element;
-        setElementCenterPositionRelative(stepfileInput, 0.35, 0.3);
-
-        let audioFileInput = DOMWrapper.create(() => {
-            return p.createFileInput(global.audioFile.load.bind(global.audioFile), "false");
-        }, "audioFileInputButton").element;
-        setElementCenterPositionRelative(audioFileInput, 0.65, 0.3);
-
-        if (isFilesReady()) {
-            let modeRadio = drawModeSelect(p);
-            if (modeRadio.value() !== "") {
-                let playButton = DOMWrapper.create(() => {
-                    return p.createButton("Play");
-                }, "playButton").element;
-                setElementCenterPositionRelative(playButton, 0.5, 0.8);
-                playButton.mouseClicked(() => {
-                    let selectedMode: Mode = getSelectedMode(modeRadio);
-                    global.simfile.finishParsing(selectedMode.id);
-                    readyPlayingDisplay(global.simfile.fullParse.tracks);
-                    PageManager.setCurrentScene(PAGES.PAGE_3);
-                });
+        let modeRadioCreateResult = DOMWrapper.create(() => {
+            return p.createRadio();
+        }, "modeRadio");
+        let modeRadio = modeRadioCreateResult.element;
+        let div = modeRadio;
+        if (!modeRadioCreateResult.alreadyExists) {
+            for (let i = 1; i <= 20; i++) {
+                // @ts-ignore
+                let option = modeRadio.option("Option" + i, i);
+                // option.style("vertical-align: middle;");
             }
+            div.style("border:2px solid #ccc;");
+            div.style("width:300px;");
+            div.style("height: 100px;");
+            div.style("overflow-y: scroll;");
+            encloseEachInputLabelPairIntoASubDiv(p, modeRadio);
+            fixRadioDivElement(modeRadio);
         }
+        setElementCenterPositionRelative(div, 0.5, 0.4);
+
+        // let stepfileInput = DOMWrapper.create(() => {
+        //     return p.createFileInput(global.simfile.load.bind(global.simfile), "false");
+        // }, "setfileInputButton").element;
+        // setElementCenterPositionRelative(stepfileInput, 0.35, 0.3);
+        //
+        // let audioFileInput = DOMWrapper.create(() => {
+        //     return p.createFileInput(global.audioFile.load.bind(global.audioFile), "false");
+        // }, "audioFileInputButton").element;
+        // setElementCenterPositionRelative(audioFileInput, 0.65, 0.3);
+        //
+        // if (isFilesReady()) {
+        //     let modeRadio = drawModeSelect(p);
+        //     if (modeRadio.value() !== "") {
+        //         let playButton = DOMWrapper.create(() => {
+        //             return p.createButton("Play");
+        //         }, "playButton").element;
+        //         setElementCenterPositionRelative(playButton, 0.5, 0.8);
+        //         playButton.mouseClicked(() => {
+        //             let selectedMode: Mode = getSelectedMode(modeRadio);
+        //             global.simfile.finishParsing(selectedMode.id);
+        //             readyPlayingDisplay(global.simfile.fullParse.tracks);
+        //             PageManager.setCurrentScene(PAGES.PAGE_3);
+        //         });
+        //     }
+        // }
+        p.pop();
+    }
+}
+
+// https://discourse.processing.org/t/how-to-organize-radio-buttons-in-separate-lines/10041/5
+function encloseEachInputLabelPairIntoASubDiv(p: p5, radioDivElement: p5.Element) {
+    const inputs = p.selectAll('input', radioDivElement.html()),
+        labels = p.selectAll('label', radioDivElement.html()),
+        len = inputs.length;
+
+    for (let i = 0; i < len; ++i)
+        p.createDiv().parent(radioDivElement).child(inputs[i]).child(labels[i]);
+}
+
+// https://discourse.processing.org/t/how-to-organize-radio-buttons-in-separate-lines/10041/5
+function fixRadioDivElement(radioDivP5Element: p5.Element) {
+    // @ts-ignore
+    radioDivP5Element._getInputChildrenArray = function () {
+        return this.elt.getElementsByTagName('input');
     }
 }
 
