7f4d5f08904b4f225afdfb0cb40ba84f569c2802
trumpet63-stream <trumpet63streaming@gmail.com>
2021-01-20 09:12:47 -0500

    note gen v3

--- src/scripts/pages/experiment/note_generator.ts
+++ src/scripts/pages/experiment/note_generator.ts
@@ -3,11 +3,17 @@ import {AccuracyObserver} from "../../accuracy_observer";
 import {Drawable} from "../../drawable";
 import {AccuracyEvent} from "../../accuracy_event";
 import {Note, NoteState, NoteType} from "../../note";
-import {clampValueToRange, getRandomIntInclusive, lerp, mapLinear, mean} from "../../util";
+import {getEmpty2dArray, getRandomIntInclusive, lerp, mean} from "../../util";
 import {global} from "../../index";
 import * as p5 from "p5";
 import {ScoreProvider} from "../../score_provider";
+import {AccuracyUtil} from "../../accuracy_util";
+import {Config} from "../../config";
 
+/* Keep a record of the note spacing (or NPS) per track, so accuracy events can be associated with the instantaneous
+ NPS. Then use that to update a player's expected NPS.
+ From Elo: New rating = old rating + K * (actual score - expected score)
+ */
 export class NoteGenerator implements AccuracyObserver, Drawable {
     private noteManager: NoteManager;
     private numTracks: number = 4;
@@ -20,6 +26,11 @@ export class NoteGenerator implements AccuracyObserver, Drawable {
     private lastNoteTrack: number = -1;
     private scoreProvider: ScoreProvider;
     private scoreMemory: number[];
+    private npsMemory: number[][]
+    private config: Config;
+    private tooEasyNps: number = 0.5;
+    private tooHardNps: number = 0.5;
+    private generatorState: number = 0;
 
     constructor(noteManager: NoteManager) {
         this.noteManager = noteManager;
@@ -27,41 +38,92 @@ export class NoteGenerator implements AccuracyObserver, Drawable {
         this.currentNoteSpacingInSeconds = this.maxNoteSpacingInSeconds;
         this.scoreProvider = new ScoreProvider(global.config, undefined);
         this.scoreMemory = [];
+        this.npsMemory = getEmpty2dArray(this.numTracks);
+        this.config = global.config;
     }
 
     public update(accuracyEvent: AccuracyEvent): void {
-        let eventScore: number = this.scoreProvider.scoreEntry(accuracyEvent);
-        this.scoreMemory.push(eventScore);
-        let maxLength = 5;
-        if (this.scoreMemory.length > maxLength) {
-            this.scoreMemory.shift();
-        }
-        let currentAverageScore = mean(this.scoreMemory);
-        let targetAverageScore = 48;
+        if (!AccuracyUtil.eventIsABoo(accuracyEvent, this.config)) {
+            let eventNps: number = this.npsMemory[accuracyEvent.trackNumber].shift();
+            let eventScore: number = this.scoreProvider.scoreEntry(accuracyEvent);
+            // console.log(eventNps.toFixed(4) + "," + eventScore.toFixed(4));
+            this.scoreMemory.push(eventScore);
+            let maxLength = 5;
+            if (this.scoreMemory.length > maxLength) {
+                this.scoreMemory.shift();
+            }
+            let averageScore = mean(this.scoreMemory);
+            let hardScore = 45;
 
-        let maxPositive = this.scoreProvider.scoreEntry({accuracyMillis: 0}) - targetAverageScore;
-        let maxNegative = -10;
-        let averageScoreDifference = currentAverageScore - targetAverageScore;
-        averageScoreDifference = clampValueToRange(averageScoreDifference, maxNegative, maxPositive);
-        console.log(averageScoreDifference);
+            // let updateIncrement = this.targetNoteSpacingInSeconds * 0.2 * this.targetNoteSpacingInSeconds;
+            // let incrementRatio = mapLinear(maxPositive, maxNegative, -1, 1, scoreDifference);
+            // let noteSpacingDelta = updateIncrement * incrementRatio;
+            let noteSpacingDelta = 0;
 
-        let updateIncrement = this.targetNoteSpacingInSeconds * 0.2 * this.targetNoteSpacingInSeconds;
-        let incrementRatio = mapLinear(maxPositive, maxNegative, -1, 1, averageScoreDifference);
-        console.log(incrementRatio);
+            switch (this.generatorState) {
+                case 0:
+                    if (averageScore > hardScore || this.scoreMemory.length < maxLength) {
+                        noteSpacingDelta -= this.targetNoteSpacingInSeconds * 0.2 * this.targetNoteSpacingInSeconds;
+                    } else {
+                        this.generatorState = 1;
+                    }
+                    break;
+                case 1:
+                    if (averageScore <= hardScore) {
+                        noteSpacingDelta += this.targetNoteSpacingInSeconds * 0.1 * this.targetNoteSpacingInSeconds;
+                    } else {
+                        this.generatorState = 2;
+                    }
+                    break;
+                case 2:
+                    if (averageScore > hardScore) {
+                        noteSpacingDelta -= this.targetNoteSpacingInSeconds * 0.05 * this.targetNoteSpacingInSeconds;
+                    } else {
+                        this.generatorState = 1;
+                    }
+                    break;
+            }
 
-        // let extraReward = 1;
-        // let extraPenalty = 2.2;
-        // if (incrementRatio > 0) {
-        //     incrementRatio *= extraPenalty;
-        // } else if (incrementRatio < 0) {
-        //     incrementRatio *= extraReward;
-        // }
+            // let maxPositive = this.scoreProvider.scoreEntry({accuracyMillis: 0}) - hardScore;
+            // let maxNegative = -10;
 
-        this.targetNoteSpacingInSeconds += updateIncrement * incrementRatio;
-        if (this.targetNoteSpacingInSeconds > this.maxNoteSpacingInSeconds) {
-            this.targetNoteSpacingInSeconds = this.maxNoteSpacingInSeconds;
+            // if (eventScore > hardScore) {
+            //     let easyConfidence = (eventScore - hardScore) / maxPositive;
+            //     if (eventNps > this.tooEasyNps) {
+            //         this.tooEasyNps = this.tooEasyNps + easyConfidence / 100 * (eventNps - this.tooEasyNps);
+            //     }
+            //     if (eventNps > this.tooHardNps) {
+            //         this.tooHardNps = this.tooHardNps + easyConfidence / 10 * (eventNps - this.tooHardNps);
+            //     }
+            // } else {
+            //     let hardConfidence = Math.max(hardScore - eventScore, hardScore) / hardScore;
+            //     if (eventNps < this.tooEasyNps) {
+            //         this.tooEasyNps = this.tooEasyNps - hardConfidence / 10 * (this.tooEasyNps - eventNps);
+            //     }
+            //     if (eventNps < this.tooHardNps) {
+            //         this.tooHardNps = this.tooHardNps - hardConfidence / 100 * (this.tooHardNps - eventNps);
+            //     }
+            // }
+            // console.log(this.tooEasyNps.toFixed(4) + " - " + this.tooHardNps.toFixed(4));
+
+            // let scoreDifference = averageScore - hardScore;
+            // scoreDifference = clampValueToRange(scoreDifference, maxNegative, maxPositive);
+
+            // let extraReward = 1;
+            // let extraPenalty = 2.2;
+            // if (incrementRatio > 0) {
+            //     incrementRatio *= extraPenalty;
+            // } else if (incrementRatio < 0) {
+            //     incrementRatio *= extraReward;
+            // }
+
+            this.targetNoteSpacingInSeconds += noteSpacingDelta;
+            if (this.targetNoteSpacingInSeconds > this.maxNoteSpacingInSeconds) {
+                this.targetNoteSpacingInSeconds = this.maxNoteSpacingInSeconds;
+            }
         }
     }
+
 
     public draw(currentTimeInSeconds: number): void {
         this.moveTowardsTargetNoteSpacing(currentTimeInSeconds);
@@ -73,6 +135,7 @@ export class NoteGenerator implements AccuracyObserver, Drawable {
             } while (nextNoteTrack == this.lastNoteTrack);
 
             this.addNote(nextNoteTrack, this.lastNoteTimeInSeconds + this.currentNoteSpacingInSeconds);
+            this.npsMemory[nextNoteTrack].push(1 / this.currentNoteSpacingInSeconds);
             this.lastNoteTrack = nextNoteTrack;
             this.lastNoteTimeInSeconds += this.currentNoteSpacingInSeconds;
         }
